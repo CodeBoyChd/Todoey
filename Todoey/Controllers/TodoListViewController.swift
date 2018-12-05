1fe0660afb72f10b503be7393419330c4c881e5b
Anurag Garg <anuraggarg@Anurags-MacBook-Pro.local>
2018-12-05 12:27:05 +0530

    - Convert to MVC architecture

--- Todoey/Controllers/TodoListViewController.swift
+++ Todoey/Controllers/TodoListViewController.swift
@@ -10,13 +10,26 @@ import UIKit
 
 class TodoListViewController: UITableViewController {
     
-    var itemArray = ["A", "B", "C"]
+    var itemArray = [Item]()
     let defaults = UserDefaults.standard
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        let newItem = Item()
+        newItem.title = "A"
+        itemArray.append(newItem)
+        
+        let newItem2 = Item()
+        newItem2.title = "B"
+        itemArray.append(newItem2)
+        
+        let newItem3 = Item()
+        newItem3.title = "C"
+        itemArray.append(newItem3)
+        
+        
         // Do any additional setup after loading the view, typically from a nib.
-        if let items = defaults.array(forKey: "TodoListArray") as? [String]{
+        if let items = defaults.array(forKey: "TodoListArray") as? [Item]{
             itemArray = items
         }
     }
@@ -29,14 +42,17 @@ class TodoListViewController: UITableViewController {
     
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: "ToDoItemCell", for: indexPath)
-        cell.textLabel?.text = itemArray[indexPath.row]
+        cell.textLabel?.text = itemArray[indexPath.row].title
+        cell.accessoryType = itemArray[indexPath.row].done ? .checkmark : .none
+        
         return cell
     }
     
     // MARK table view delegate methods
-    
     override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
         print("\(itemArray[indexPath.row])")
+        
+        itemArray[indexPath.row].done = !itemArray[indexPath.row].done
         
         // to deselect the item after it gets clicked
         tableView.deselectRow(at: indexPath, animated: true)
@@ -57,7 +73,9 @@ class TodoListViewController: UITableViewController {
         let alert = UIAlertController(title: "Add new Todoey item", message: "", preferredStyle: .alert)
         let action = UIAlertAction(title: "Add item", style: .default) { (action) in
             // what will happen whenever user clicks the ADD item on our UI Alert
-            self.itemArray.append(textField.text!)
+            let newItem = Item()
+            newItem.title = textField.text!
+            self.itemArray.append(newItem)
             
             // save in the database
             self.defaults.set(self.itemArray, forKey: "TodoListArray")
---
+++ Todoey/Data Model/Item.swift
@@ -0,0 +1,14 @@
+//
+//  Item.swift
+//  Todoey
+//
+//  Created by Anurag Garg on 05/12/18.
+//  Copyright © 2018 U2opia Mobile. All rights reserved.
+//
+
+import Foundation
+
+class Item {
+    var title : String = ""
+    var done : Bool = false
+}
